4f35b02087ad5905f2bf24c4596a2f4f4d889b81
Chaitanya Jeet singh <chaitanyajeetsingh@gmail.com>
2026-06-26 17:33:30 +0530

    2nd commit

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,7 +1,27 @@
-﻿namespace ConsoleApp2
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Runtime.Intrinsics.X86;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
+            public static void operation(int i , int y )
+            {
+                Console.WriteLine($"Addition =  {i + y}");
+                Console.WriteLine($"Difference  {i - y}");
+                Console.WriteLine($"Product =  {i * y}");
+                Console.WriteLine($"Division =  {i / y}");
+            }
+              
+            public static void operation(double i , double y )
+            {
+                Console.WriteLine($"Addition =  {i + y}");
+                Console.WriteLine($"Difference  {i - y}");
+                Console.WriteLine($"Product =  {i * y}");
+                Console.WriteLine($"Division =  {i / y}");
+            }
         static void Main(string[] args)
         {
             ////Console.WriteLine("Hello, \rWorld!");
@@ -22,12 +42,56 @@
             //Console.WriteLine($"The hypotenuous is {hy:f2}");
 
 
-            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+            //}
 
-            foreach (int i in numbers)
-            {
-                Console.Write(i);
-            }
+
+
+            //<-----------------------------------Day-2------------------------------------->
+
+
+
+            //int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.Write(i);
+
+
+            //}
+
+
+            //            Exercise 1: Multi - Dimensional Array Iteration(The Grid)
+            //Scenario: You are building a seating chart for a small theater.The theater has 3 rows, and each row has 4 seats.
+
+            //Create a 2D array(int[,]) to represent the theater.
+
+            //Initialize the seats with ticket prices: Row 0 costs $10, Row 1 costs $20, and Row 2 costs $30.
+
+            //Use nested for loops to print out the pricing grid.
+
+
+            //int[,] seating = { { 10,10,10,10},
+            //                   {20,20,20,20 },
+            //                   { 30,30,30,30}
+            //                                };
+            //for(int i = 0; i<3;i++) 
+            //{
+            //    for(int j = 0; j < 4; j++)
+            //    {       
+            //        Console.Write(seating[i,j]+ "\t");
+            //    }
+            //    Console.WriteLine("\n");
+                
+            
+            //}
+
+
+            //METHOD OVERLOADING
+
+
+            operation(10.2, 2.3);
+
+
             Console.ReadKey();
         }
     }
--- ConsoleApp2/ConsoleApp2/obj/Release/net10.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/ConsoleApp2/obj/Release/net10.0/ConsoleApp2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf4456bfb843731fa70c53421e26fdb85e037822")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
